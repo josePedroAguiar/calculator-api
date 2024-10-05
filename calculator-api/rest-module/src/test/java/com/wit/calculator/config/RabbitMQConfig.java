e0dda485e2a7b7a22c04977c40096884517f4fce
José Aguiar <jpaguiar17@gmail.com>
2024-10-05 16:38:06 +0100

    Fix & Add: Controller and Config to RestModule

---
+++ calculator-api/rest-module/src/test/java/com/wit/calculator/config/RabbitMQConfig.java
@@ -0,0 +1,14 @@
+package com.wit.calculator.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue calculatorQueue() {
+        return new Queue("calculator_queue", false);
+    }
+}
