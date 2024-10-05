f87631f4ca16f8755de2e533a9acc6063b87a96d
José Aguiar <jpaguiar17@gmail.com>
2024-10-05 17:07:09 +0100

    Fix Previous Commit

--- calculator-api/calculator-module/src/main/java/com/wit/calculator/CalculatorModuleApplication.java
+++ calculator-api/calculator-module/src/main/java/com/wit/calculator/CalculatorModuleApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorModuleApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.setProperty("server.port", "8081");
         SpringApplication.run(CalculatorModuleApplication.class, args);
     }
 
